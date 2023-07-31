0a44ea5dfc29cc3e79049ad6ac4d694b3e581f8b
A1ex-Fu <alex.aj.fu@gmail.com>
2023-07-31 13:45:02 -0700

    finish text buf

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -81,9 +81,7 @@ impl Writer {
 
     //came like this not sure if i actually need it
     fn new_line(&mut self) {/* TODO */}
-}
 
-impl Writer {
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
             match byte {
