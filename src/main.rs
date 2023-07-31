7d1e24082c7cb13be12d087630e148641a3743f4
A1ex-Fu <alex.aj.fu@gmail.com>
2023-07-31 13:29:22 -0700

    text buffer main

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,23 @@ static HELLO: &[u8] = "Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
+// #[no_mangle]
+// pub extern "C" fn _start() -> ! {
+//     let vga_buffer = 0xb8000 as *mut u8;
+
+//     for (i, &byte) in HELLO.iter().enumerate() {
+//         unsafe {
+//             *vga_buffer.offset(i as isize * 2) = byte;
+//             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+//         }
+//     }
+
+//     loop {}
+// }
 
 /// This function is called on panic.
 #[panic_handler]
