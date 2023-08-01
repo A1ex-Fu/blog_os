fcab58a1b508e6972d211b6c30d2e9ea598c066c
A1ex-Fu <alex.aj.fu@gmail.com>
2023-07-31 17:26:06 -0700

    test

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,8 @@ fn panic(info: &PanicInfo) -> ! {
 
 mod vga_buffer;
 
-#[feature(custom_test_frameworks)]
-#[test_runner(crate::test_runner)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
