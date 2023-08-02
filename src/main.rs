a1e59451806c03d7c97540e882356e3cbd54a768
A1ex-Fu <alex.aj.fu@gmail.com>
2023-08-02 12:22:58 -0700

    test

--- src/main.rs
+++ src/main.rs
@@ -71,3 +71,22 @@ fn trivial_assertion() {
     assert_eq!(0, 1);
     serial_println!("[ok]");
 }
+
+
+// our existing panic handler
+#[cfg(not(test))] // new attribute
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
