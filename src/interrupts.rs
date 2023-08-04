4c0eab4097112cb613a032eeccf9e90ae131e997
A1ex-Fu <alex.aj.fu@gmail.com>
2023-08-03 17:29:27 -0700

    test

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -22,6 +22,11 @@ extern "x86-interrupt" fn breakpoint_handler(
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame, _error_code: u64) -> !
+{
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
 
 
 #[test_case]
