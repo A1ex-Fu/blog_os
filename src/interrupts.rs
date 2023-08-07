9546628ee1ccd874977dcbd10d677f3584690f76
A1ex-Fu <alex.aj.fu@gmail.com>
2023-08-07 14:30:33 -0700

    test

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -40,7 +40,7 @@ extern "x86-interrupt" fn breakpoint_handler(
 extern "x86-interrupt" fn double_fault_handler(
     stack_frame: InterruptStackFrame, _error_code: u64) -> !
 {
-    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+ //   panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
 
