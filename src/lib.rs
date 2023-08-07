82b5b48ee7228f060e7fe33ebe58de94198e9e24
A1ex-Fu <alex.aj.fu@gmail.com>
2023-08-07 14:32:43 -0700

    test

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub fn init() {
     gdt::init();
     interrupts::init_idt();
     unsafe{interrupts::PICS.lock().initialize()};
-    x86_64::instructions::interrupts::enable();  
+   // x86_64::instructions::interrupts::enable();  
 }
 
 
