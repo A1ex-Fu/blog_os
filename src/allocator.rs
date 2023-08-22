857cc2866c793f84e759d4a3f608f5f3f860485d
A1ex-Fu <alex.aj.fu@gmail.com>
2023-08-22 14:21:48 -0700

    working on allocator designs

--- src/allocator.rs
+++ src/allocator.rs
@@ -66,6 +66,22 @@ pub fn init_heap(
 }
 
 /// A wrapper around spin::Mutex to permit trait implementations.
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
 
 
 
--- src/allocator/bump.rs
+++ src/allocator/bump.rs
@@ -37,22 +37,6 @@ impl BumpAllocator {
 
 // }
 
-pub struct Locked<A> {
-    inner: spin::Mutex<A>,
-}
-
-impl<A> Locked<A> {
-    pub const fn new(inner: A) -> Self {
-        Locked {
-            inner: spin::Mutex::new(inner),
-        }
-    }
-
-    pub fn lock(&self) -> spin::MutexGuard<A> {
-        self.inner.lock()
-    }
-}
-
 
 
 
