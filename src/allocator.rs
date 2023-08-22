dd3fb38be6843b6ffd8de8888978fe27b325be29
A1ex-Fu <alex.aj.fu@gmail.com>
2023-08-22 14:25:19 -0700

    working on allocator designs

--- src/allocator.rs
+++ src/allocator.rs
@@ -13,20 +13,20 @@ use linked_list_allocator::LockedHeap;
 
 pub mod bump;
 
-#[global_allocator]
+// #[global_allocator]
 // static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 
 
-// unsafe impl GlobalAlloc for Dummy {
-//     unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
-//         null_mut()
-//     }
+unsafe impl GlobalAlloc for Dummy {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        null_mut()
+    }
 
-//     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-//         panic!("dealloc should be never called")
-//     }
-// }
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        panic!("dealloc should be never called")
+    }
+}
 
 use x86_64::{
     structures::paging::{
