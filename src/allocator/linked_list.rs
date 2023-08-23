52180a5a64178cfa39b970a5aa297334a7728cb4
A1ex-Fu <alex.aj.fu@gmail.com>
2023-08-22 18:10:27 -0700

    working on allocator designs

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -6,32 +6,6 @@ use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr;
 
 unsafe impl GlobalAlloc for Locked<LinkedListAllocator> {
-        /// Looks for a free region with the given size and alignment and removes
-    /// it from the list.
-    ///
-    /// Returns a tuple of the list node and the start address of the allocation.
-    fn find_region(&mut self, size: usize, align: usize)
-        -> Option<(&'static mut ListNode, usize)>
-    {
-        // reference to current list node, updated for each iteration
-        let mut current = &mut self.head;
-        // look for a large enough memory region in linked list
-        while let Some(ref mut region) = current.next {
-            if let Ok(alloc_start) = Self::alloc_from_region(&region, size, align) {
-                // region suitable for allocation -> remove node from list
-                let next = region.next.take();
-                let ret = Some((current.next.take().unwrap(), alloc_start));
-                current.next = next;
-                return ret;
-            } else {
-                // region not suitable -> continue with next region
-                current = current.next.as_mut().unwrap();
-            }
-        }
-
-        // no suitable region found
-        None
-    }
     
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         // perform layout adjustments
@@ -141,9 +115,33 @@ impl LinkedListAllocator {
         // region suitable for allocation
         Ok(alloc_start)
     }
-}
 
-impl LinkedListAllocator {
+        /// Looks for a free region with the given size and alignment and removes
+    /// it from the list.
+    ///
+    /// Returns a tuple of the list node and the start address of the allocation.
+    fn find_region(&mut self, size: usize, align: usize)
+        -> Option<(&'static mut ListNode, usize)>
+    {
+        // reference to current list node, updated for each iteration
+        let mut current = &mut self.head;
+        // look for a large enough memory region in linked list
+        while let Some(ref mut region) = current.next {
+            if let Ok(alloc_start) = Self::alloc_from_region(&region, size, align) {
+                // region suitable for allocation -> remove node from list
+                let next = region.next.take();
+                let ret = Some((current.next.take().unwrap(), alloc_start));
+                current.next = next;
+                return ret;
+            } else {
+                // region not suitable -> continue with next region
+                current = current.next.as_mut().unwrap();
+            }
+        }
+
+        // no suitable region found
+        None
+    }
     /// Adjust the given layout so that the resulting allocated memory
     /// region is also capable of storing a `ListNode`.
     ///
