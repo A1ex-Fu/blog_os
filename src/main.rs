21e83370b9cda0b019ea270b7963d6780c3663b0
alex <alex.aj.fu@gmail.com>
2023-08-30 18:03:32 -0700

    working on async

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 
 
-    let mut executor = SimpleExecutor::new();
+    let mut executor = Executor::new();
     executor.spawn(Task::new(example_task()));
     executor.spawn(Task::new(keyboard::print_keypresses()));
     executor.run();
--- src/task/mod.rs
+++ src/task/mod.rs
@@ -1,20 +1,24 @@
-use core::{future::Future, pin::Pin};
 use alloc::boxed::Box;
+use core::{
+    future::Future,
+    pin::Pin,
+    sync::atomic::{AtomicU64, Ordering},
+    task::{Context, Poll},
+};
 
-pub mod simple_executor;
+pub mod executor;
 pub mod keyboard;
-
+pub mod simple_executor;
 
 pub struct Task {
+    id: TaskId,
     future: Pin<Box<dyn Future<Output = ()>>>,
 }
-
-
-use core::task::{Context, Poll};
 
 impl Task {
     pub fn new(future: impl Future<Output = ()> + 'static) -> Task {
         Task {
+            id: TaskId::new(),
             future: Box::pin(future),
         }
     }
@@ -24,7 +28,12 @@ impl Task {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct TaskId(u64);
 
-
-
-
+impl TaskId {
+    fn new() -> Self {
+        static NEXT_ID: AtomicU64 = AtomicU64::new(0);
+        TaskId(NEXT_ID.fetch_add(1, Ordering::Relaxed))
+    }
+}
